b40571aba6575128dfd7fdc1fbca443348b22058
KruzadeR-VictoR <victorbiju31@gmail.com>
2022-07-03 21:29:14 +0530

    added calculator operations

--- script.js
+++ script.js
@@ -3,43 +3,58 @@ const getNumber = (evt) => {
   if (charCode > 31 && (charCode < 48 || charCode > 57)) return false;
 };
 
-// const bill = document.querySelector(".bill-amt");
-// let para = [];
+const bill = document.querySelector(".bill-amt");
+let para = [];
 
-// bill.addEventListener("change", (e) => {
-//   const billAmt = e.target.value;
-//   console.log(billAmt);
-//   para.push(billAmt);
-// });
+bill.addEventListener("change", (e) => {
+  const billAmt = e.target.value;
+  console.log(billAmt);
+  para.push(billAmt);
+});
 
-// const tips = document.querySelectorAll(".tip-amount");
-// const arr = [...tips];
+const tips = document.querySelectorAll(".tip-amount");
+const arr = [...tips];
 
-// arr.map((tip) => {
-//   tip.addEventListener("click", (e) => {
-//     const tipPercent = e.target.value;
-//     para.push(tipPercent);
-//   });
-// });
+arr.map((tip) => {
+  tip.addEventListener("click", (e) => {
+    const tipPercent = e.target.value;
+    para.push(tipPercent);
+  });
+});
 
-// const people = document.querySelector(".people-no");
+const people = document.querySelector(".people-no");
 
-// people.addEventListener("change", (e) => {
-//   const peopleNo = e.target.value;
-//   console.log(peopleNo);
-//   para.push(peopleNo);
-// });
-// console.log(para);
+people.addEventListener("change", (e) => {
+  const peopleNo = e.target.value;
+  console.log(peopleNo);
+  para.push(peopleNo);
+  calculate(...para);
+});
+console.log(para);
 
-// let paraObj = Object.assign(para);
+let tipAmt = document.querySelector(".Tip-person");
+let totalAmt = document.querySelector(".Tip-total");
 
-// const calculate = (billAmt, tipPercent, peopleNo) => {
-//   //   let tipAmtPerPerson = (billAmt * (tipPercent / 100)) / peopleNo;
-//   let tipAmtPerPerson = eval(billAmt * tipPercent);
-//   console.log(tipAmtPerPerson);
-// };
+const calculate = (...para) => {
+  let tipAmtPerPerson = (para[0] * (para[1] / 100)) / para[2];
+  let totalPerPerson = para[0] / para[2] + tipAmtPerPerson;
+  console.log(tipAmtPerPerson, totalPerPerson);
 
-var BillAmt = document.getElementsByClassName("tip-amt").value;
-var TipPercent = document.getElementById("bill-amt").value;
-var people= document.getElementById("bill-amt").value;
-console.log(BillAmt)
+  tipAmt.innerHTML = tipAmtPerPerson;
+  totalAmt.innerHTML = totalPerPerson;
+};
+
+//| Reset
+
+const reset = document.querySelector(".Reset");
+reset.addEventListener("click", () => {
+  bill.value = "";
+  people.value = "";
+  tipAmt.innerHTML = "0";
+  totalAmt.innerHTML = "0";
+});
+
+// var BillAmt = document.getElementsByClassName("tip-amt").value;
+// var TipPercent = document.getElementById("bill-amt").value;
+// var people= document.getElementById("bill-amt").value;
+// console.log(BillAmt)
